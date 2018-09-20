5b5c2d22b21455689117a0aa4a04b1dec54374b0
kyle355469 <kyle355469@gmail.com>
2018-09-20 09:05:40 +0800

    Update 階乘計算.cpp

--- 階乘計算.cpp
+++ 階乘計算.cpp
@@ -1,24 +1,28 @@
 #include <iostream> 
 #include <math.h>
+#include <stdlib.h>
 using namespace std; 
 
 int main() { 
 	int factorial;     // factorial 階乘
 	cin >> factorial;
 	int digits;		// 正在乘的數字 
-	int total[100];    //總數 
+	int total[70];    //總數 
+	for(int y = 0; y <= 70; y++){
+		total[y] = 0;
+	}
 	total[0] = 1;
 	int check;        //檢查最高位數 
-	for(int i = 0; i < factorial; i++) {
+	for(int i = 1; i <= factorial; i++) {
 		digits = i;
 		
 		
-		for(int x = 0; x < 3; x++){
-			for(int y = 0; y < 100; y++){
-				total[y] = total[y] * digits;
-			}
+		
+		for(int y = 0; y < 70; y++){
+			total[y] = total[y] * digits;
 		}
-		for(int y = 0; y < 100; y++){
+		
+		for(int y = 0; y < 70; y++){
 			if(total[y] > 10){
 				total[y+1] = (total[y] / 10);
 				total[y] = (total[y] % 10);
@@ -27,9 +31,10 @@ int main() {
 		
 	}
 	
-	for(int y = 0; y < 100; y++){
-		if(total[y] != 0 && total[y+1] == 0){
+	for(int y = 70; y >=0; y++){
+		if(total[y] == 0 && total[y-1] != 0){
 			check = y;
+			break;
 		}	
 	}
 	//cout << sizeof(total) << endl;
